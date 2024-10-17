1787a1b61d162c31c65f7c71536d6b3739a9a63d
lijianfeng <2634307978@qq.com>
2024-10-17 18:46:51 +0800

    123\UI菜单1

--- Assets/Scripts/ItemAndInventory/InventoryManager.cs
+++ Assets/Scripts/ItemAndInventory/InventoryManager.cs
@@ -2,10 +2,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
-using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
