eb0f84a4b03a4695738c7a34027a4968297bda28
lijianfeng <2634307978@qq.com>
2024-10-21 10:18:56 +0800

    UI菜单按钮

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,21 @@
+//UI脚本控制
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    //切换面板
+    public void SwitchTo(GameObject _menu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            //先关闭显示所有面板显示
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        if (_menu != null)
+        {
+            //显示
+            _menu.SetActive(true);
+        }
+    }
+}
